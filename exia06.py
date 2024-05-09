8086a758507360d772bd357e040366f0f7cb6bd2
SamuelMends <samuelqmendes@gmail.com>
2024-05-09 20:16:24 -0300

    exia07.py

--- exia06.py
+++ exia06.py
@@ -1,7 +1,7 @@
 '''
 Escreva um algoritimo que lê 5 valores para a variável A, um de cada vez, e conta quantos destes valores são negativos. Após a leitura, o programa deve mostrar a quantidade de números negativos
 '''
-
+#Exia 06:
 
 cont = 0
 for c in range (0,5,):
