567601242daad00126c36e4f410cdca5d8e6ebb2
SamuelMends <samuelqmendes@gmail.com>
2024-05-12 18:53:43 -0300

    exercicio059

--- exercicio059.py
+++ exercicio059.py
@@ -1,26 +1,30 @@
 primeiro = int(input('Primeiro valor: '))
 segundo = int(input('Segundo valor: '))
-print('''[ 1 ] somar
+opcao = 0
+while opcao != 5:      
+    print('''[ 1 ] somar
 [ 2 ] multiplicar
 [ 3 ] maior
 [ 4 ] novos números
 [ 5 ] sair do programa''')
-opcao = int(input('Qual sua opção? '))
-if opcao == 5:
+    opcao = int(input('Qual sua opção? '))                         
+    if opcao == 5:
         print('-'* 20)
         print('Finalizando...')
         print('-'* 20)
-elif opcao == 4:
+    elif opcao == 4:
+        print('Informe os números novamente: ')
         primeiro = int(input('Primeiro valor: '))
         segundo = int(input('Segundo valor: '))
-elif opcao == 3:
+    elif opcao == 3:
         if primeiro > segundo:
                 print('O primeiro número é maior do que o segundo.')
         else:
                 print('O segundo número é maior!')
-elif opcao == 2:
+    elif opcao == 2:
         calculom = primeiro * segundo
         print('O valor da multiplicação entre {} e {} é de {}'.format(primeiro,segundo,calculom))
-elif opcao == 1:
+    elif opcao == 1:
         calculos = primeiro + segundo
         print('O valor da soma entre {} e {} é de {}'.format(primeiro,segundo,calculos))
+    
