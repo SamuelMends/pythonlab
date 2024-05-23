ac074dc7c30ad37c71de2149b901b194f0e003c2
SamuelMends <samuelqmendes@gmail.com>
2024-05-23 19:52:23 -0300

    Update py.py

--- py.py
+++ py.py
@@ -8,3 +8,20 @@
 #Uso correto dos processos de raciocinio na programação de computadores  
 
 #Algoritimo, é uma sequência de passos.
+
+#Crie um algoritimo para fazer a troca de pneu de um carro. Considere as seguintes situações:
+#Se o macaco estiver estragado.
+#Se o pneu reserva estiver vazio
+#Se dois pneus do carro estiverem furados
+
+#Verificar se o carro está completamente parado
+#Se o carro estiver parado 
+#Pegar o macaco para erguer o carro
+#Se o macaco estiver bom, erguer o carro
+#Se o macaco estiver ruim, parar e ligar para emergencia.
+
+
+
+
+
+
