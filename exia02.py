06ee2d3f4baae2b1b6e987f1087aa834475f6640
SamuelMends <samuelqmendes@gmail.com>
2024-05-24 19:47:07 -0300

    Create exia02.py

---
+++ exia02.py
@@ -0,0 +1,7 @@
+#Leia um número interiro e informe se ele é positivo ou negativo (considere zero como positivo)
+
+num = int(input('Digite um número: '))
+if num >= 0:
+    print(f'O número {num} é positivo!')
+else:
+    print(f'O número {num} é negativo.')
