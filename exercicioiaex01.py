4c75022b001c476d9dd314bb8d65b2ca58c84792
SamuelMends <samuelqmendes@gmail.com>
2024-05-24 19:30:06 -0300

    Update exercicioiaex01.py

--- exercicioiaex01.py
+++ exercicioiaex01.py
@@ -1,8 +1,11 @@
-nota1 = float(input('Digite sua primeira nota: '))
-nota2 = float(input('Digite sua segunda nota: '))
-med = (nota1 + nota2)/2
-if med > 4:
-    print(f'Parabéns você foi aprovado! Sua média foi de {med}')
-else:
-    print(f'Infelizmente você não passou sua média foi de {med}')
+nota1 = float(input('Digite sua em português: '))
+nota2 = float(input('Digite sua em matemática: '))
+nota3 = float(input('Digite sua história: '))
+med = (nota1 + nota2 + nota3)/3
+if med > 8:
+    print('Parabéns você foi aprovado! Sua média foi de {:.2f}'.format(med))
+elif med < 8 and med > 4:
+    print('Você ficou de recuperação pois sua média foi de {:.2f}'.format(med))
+elif med < 4:
+    print('Infelizmente você não passou sua média foi de {:.2f}'.format(med))
 print('Até a próxima!')
