f065ce39af23d6ac4ecf41ae889d911ad5d272fb
SamuelMends <samuelqmendes@gmail.com>
2024-05-24 19:54:45 -0300

    Create exia03.py

---
+++ exia03.py
@@ -0,0 +1,11 @@
+#Leia dois números inteiros e informe qual deles é o maior. Se os npumeros forem iguais
+#mostrar esta informação na tela:
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite mais um número: '))
+if num1 > num2:
+    print('O primeiro número é maior')
+elif num2 > num1:
+    print('O segundo número é o maior')
+elif num1 == num2:
+    print('Os números são iguais')
