c8b47005ccd7f07ed9b411788493eda3d0f9e4a5
SamuelMends <samuelqmendes@gmail.com>
2024-05-25 16:42:05 -0300

    Update exia04.py

--- exia04.py
+++ exia04.py
@@ -9,20 +9,6 @@ cal = (num * num)
 print(f'O quadrado desse número é {cal}')
 
 '''
-'''
-N = False
-while resp == 'S':
-    con = num + cont
-    cal = (con * con)
-    print(f'O quadrado do proximo número {con} número é {cal}')
-print('Programa finalizado!')
-'''
-'''
-
-for num in range (15,201,):
-    print(f'O resultado do quadrado de {num} é igual a {num*num}'
-'''
-
 
 cont = 1
 num = int(input('Digite um número inteiro: '))
