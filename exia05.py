c6b3defd0065650a8ee9350fdd7b99355c3e882c
SamuelMends <samuelqmendes@gmail.com>
2024-05-25 13:47:46 -0300

    Create exia05.py

---
+++ exia05.py
@@ -0,0 +1,7 @@
+'''
+Apresente o total da soma obtida dos cem primeiros número inteiros de 1 até 100
+'''
+
+for c in range (0,100,):
+    soma = c + c
+    print(f'A soma de do range de 1 a 100 é de {soma}')
