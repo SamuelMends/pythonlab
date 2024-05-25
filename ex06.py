e1ab3ed7f81c8abd42d3258091abf0f13659231a
SamuelMends <samuelqmendes@gmail.com>
2024-05-25 14:26:25 -0300

    main

---
+++ ex06.py
@@ -0,0 +1,11 @@
+'''
+Escreva um algoritimo que lê 5 valores para a variável A, um de cada vez, e conta quantos destes valores são negativos. Após a leitura, o programa deve mostrar a quantidade de números negativos
+'''
+
+
+cont = 0
+for c in range (0,5,):
+    num = int(input('Digite um número: '))
+    if num < 0:
+        cont +=1  
+print(f'O total de números negativos foi de {cont}')
