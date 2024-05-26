b472edc9276b81865c71315f4416cc939e2128d3
SamuelMends <samuelqmendes@gmail.com>
2024-05-26 20:48:02 -0300

    commando

---
+++ exia09.py
@@ -0,0 +1,14 @@
+import numpy
+
+vetor = numpy.empty(6)
+vetor [0] = 10
+vetor [1] = 20
+vetor [2] = 30
+vetor [3] = 40
+vetor [4] = 50
+vetor [5] = 60
+
+print(vetor[5])
+
+for c in range(0,6):
+    print(vetor[c])
---
+++ listasetuplas.py
@@ -0,0 +1,3 @@
+#Diferença entre Tuplas e Listas:
+#Tuplas são construidas com parênteses () e não são mutáveis por comando
+#Listas são construidas com colchetes [] e são mutávies por comando
