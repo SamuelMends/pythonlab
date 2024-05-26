44dbc7b577a7e8116d77229d98d364f8a1c0382b
SamuelMends <samuelqmendes@gmail.com>
2024-05-26 19:10:21 -0300

    Update exia05.py

--- exia05.py
+++ exia05.py
@@ -5,4 +5,6 @@ Apresente o total da soma obtida dos cem primeiros número inteiros de 1 até 10
 for c in range (0,100,):
     cont = c + 1
     soma = c + c
-    print(f'A soma de {c} + {cont} é de {soma + 1}')
+    results = soma + soma
+    print(f'Somatório dos 100 primeiros números é {results}')
+    #print(f'A soma de {c} + {cont} é de {soma + 1}')
