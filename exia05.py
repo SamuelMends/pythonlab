8aff217d53cd35978a7c8bdae34cc707bbcfab5f
SamuelMends <samuelqmendes@gmail.com>
2024-05-26 19:17:05 -0300

    Update exia05.py

--- exia05.py
+++ exia05.py
@@ -1,10 +1,17 @@
 '''
-Apresente o total da soma obtida dos cem primeiros número inteiros de 1 até 100
+Apresente o total da soma obtida dos cem primeiros número inteiros de 1 até 100 (Acumulador)
 '''
 
-for c in range (0,100,):
-    cont = c + 1
-    soma = c + c
-    results = soma + soma
-    print(f'Somatório dos 100 primeiros números é {results}')
+#for c in range (0,100,):
+   # cont = c + 1
+    #soma = c + c
+    #results = soma + soma
+    #print(f'Somatório dos 100 primeiros números é {results}')
     #print(f'A soma de {c} + {cont} é de {soma + 1}')
+
+contador = 1
+soma = 0
+while contador <=100:
+    soma = soma + contador
+    contador += 1
+    print(f'Somatório dos 100 primeiros números: {soma}')
