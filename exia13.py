8d6f2cad5c3fed0b2261c9b7880644240166a78a
SamuelMends <samuelqmendes@gmail.com>
2024-05-27 20:39:52 -0300

    Create exia13.py

---
+++ exia13.py
@@ -0,0 +1,13 @@
+#Leia uma matriz de inteiros 3x3. Após leia um valor individual e mostre quantas vezes o número #digitado aparece na matriz
+
+import numpy
+matriz = numpy.empty([3,3])
+for linha in range (0,3):
+    for coluna in range(0,3):
+        data = int(input(f'Digite o valor da linha {linha} e da coluna {coluna}: '))
+        matriz [linha][coluna] = data
+pesq = int(input('Digite o valor individual: '))
+if pesq in matriz:
+    print('Valores encontrados!')
+else:
+    print('Valores não encontrados!')
