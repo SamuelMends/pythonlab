49f128e65ba441ccffb784b9fac2d84945ed362d
SamuelMends <samuelqmendes@gmail.com>
2024-05-27 19:57:52 -0300

    Create matriz.py

---
+++ matriz.py
@@ -0,0 +1,11 @@
+#Matrizes:
+#Vetor com mais de uma dimensão: (Colunas e Linhas)
+
+#Tipo de dado para representar uma coleção de variáveis do mesmo tipo: (Apenas um tipo de dado)
+
+#Estrutura homogênea: (Int) (Float) (String) e etc
+
+#É uma série de variáveis do mesmo tipo referenciada pelo mesmo nome (0 ao infinito)
+
+#Cada variável é diferenciada por dois índices: O primeiro representa a LINHA e o segundo representa
+#a COLUNA.
