6e360c19dad1f8fc2d4378c8714bc818ebb3b8ab
SamuelMends <samuelqmendes@gmail.com>
2024-05-27 20:13:13 -0300

    Update matrizpt1.py

--- matrizpt1.py
+++ matrizpt1.py
@@ -5,9 +5,8 @@ matriz[0][0] = 3
 matriz[0][1] = 6
 matriz[1][0] = 9
 matriz[1][1] = 21
-print(matriz[1][0])
 
 for linha in range(0,2):
-    print(f'{linha}')
+    print(matriz[linha])
 for coluna in range(0,2):
-    print(f'{coluna}')
+    print(matriz[coluna])
