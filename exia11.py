c702e2464aadcb41b29ecbfe8cc56e460fa39791
SamuelMends <samuelqmendes@gmail.com>
2024-05-26 22:34:52 -0300

    Create exia11.py

---
+++ exia11.py
@@ -0,0 +1,14 @@
+#Leia um vetor A com 10 elementos do tipo real. Construir um vetor B de mesmo tipo, 
+#Sendo que cada elemento do vetor B deve ser o cubo de cada elemento correspondente do vetor A
+#Apresentar no final os dois valores.
+
+import numpy
+vetora = numpy.empty(10)
+vetorb = numpy.empty(10)
+for m in range (0,10):
+    q = int(input(f'Digite o {m+1}º valor: '))
+    vetora[m] = q
+cont = 1
+for vetorb in range(0,10):
+    vetorb = (vetora[m]**2)
+print(f'O valor dos vetores do tipo A são {vetora} e seus respectivos quadrados {vetorb}')
