1e3ac65f76db9864fdf09df9afcb0967e8254fc8
SamuelMends <samuelqmendes@gmail.com>
2024-05-27 21:40:57 -0300

    Update exia14.py

--- exia14.py
+++ exia14.py
@@ -10,7 +10,9 @@ for linha in range(0,4):
     for coluna in range(0,4):
         data = int(input(f'Digite o valor referente a linha {linha} e a coluna {coluna}: '))
         matriz[linha][coluna] = data
-        somalinha = matriz[2][0]+matriz[2][1]+ matriz[2][2]+matriz[2][3]
+        somalinha = matriz[2][0]+matriz[2][1]+matriz[2][2]+matriz[2][3]
+        somacoluna = matriz[0][2]+matriz[1][2]+matriz[2][2]+matriz[3][2]
+        somatotal = numpy.sum(matriz)
 print(f'O somatório da segunda linha possui o total de {somalinha}')
-print(f'O somatório da segunda coluna')
-    
+print(f'O somatório da segunda coluna possui o total de {somacoluna}')
+print(f'O somatório total da matriz é de {somatotal}')
