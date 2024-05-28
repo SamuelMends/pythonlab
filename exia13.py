07a4ef7f3880d02c66fe0343ca5faf45d7d745a8
SamuelMends <samuelqmendes@gmail.com>
2024-05-27 22:13:11 -0300

    Update exia13.py

--- exia13.py
+++ exia13.py
@@ -7,7 +7,9 @@ for linha in range (0,3):
         data = int(input(f'Digite o valor da linha {linha} e da coluna {coluna}: '))
         matriz [linha][coluna] = data
 pesq = int(input('Digite o valor individual: '))
-if pesq in matriz:
-    print(f'Valores encontrados na posição {linha}')
-else:
-    print('Valores não encontrados!')
+q = 0
+for linha in range (0,3):
+    for coluna in range(0,3):
+        if matriz[linha][coluna] == pesq:
+            q += 1
+print(f'Quantidade do valor é de {q} vezes')
