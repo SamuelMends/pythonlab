1ee3b8c249d8201462b8b1b8f00793b85c919770
SamuelMends <samuelqmendes@gmail.com>
2024-05-28 18:01:38 -0300

    Create funcao.py

---
+++ funcao.py
@@ -0,0 +1,9 @@
+#FUNÇÃO
+# É uma das principais caracteristicas da programação
+# Divisão de problemas complexos em pequenos problemas 
+# É um pequeno programa dentro de outro programa (Programas aninhados)
+# Trechos de programa que recebem um determinado nome e podem ser chamadas varias vezes durante a execução 
+# Principais vantagens:
+# Reutilização de código
+# Modularidade
+# Facilidade de manutenção do sistema
