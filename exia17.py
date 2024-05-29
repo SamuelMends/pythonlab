be4617df5422ea5d62339133a61b53e3c2541d88
SamuelMends <samuelqmendes@gmail.com>
2024-05-28 21:58:41 -0300

    Create exia17.py

---
+++ exia17.py
@@ -0,0 +1,7 @@
+# Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. 
+# A fórmula de conversão é F = (9 * C + 160) / 5, na qual F é a temperatura em Fahrenheit
+# e C é a temperatura em graus Celsius
+
+# Função para ler os valores:
+# Função para fazer o cálculo:
+# Função para mostrar o resultado:
