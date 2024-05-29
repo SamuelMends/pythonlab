d0fd3e7387eea5cd74fa53137e89a0e303627ac6
SamuelMends <samuelqmendes@gmail.com>
2024-05-28 21:55:29 -0300

    Update exia16.py

--- exia16.py
+++ exia16.py
@@ -1,10 +1,14 @@
 #Crie uma função que imprima na tela a palavra "Programação" 10 vezes:
 
-cont = 0
-
 
 def prog():
     print('\nProgramação'*10)
 
 
 prog()
+
+cont = 1
+
+for program in range(0,10):
+    cont +=1
+    print(f'Programação {cont-1}º')
