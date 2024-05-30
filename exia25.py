2fb059984229be315b625aaf97a4a69e6cbd2003
SamuelMends <samuelqmendes@gmail.com>
2024-05-30 19:39:42 -0300

    Create exia25.py

---
+++ exia25.py
@@ -0,0 +1,13 @@
+# Ler dois números inteiros, executar e mostrar o resultado das seguintes operações: adição, subtração, multiplicação e divisão
+
+a = 12
+b = 10
+#Adição
+print(f'A soma de {a} + {b} é igual a {a+b}')
+#Subtração
+print(f'A subtração entre {a} - {b} é igual a {a-b}')
+#Multiplicação
+print(f'A multiplicação entre {a} e {b} é igual a {a*b}')
+#Divisão
+print(f'A divisão entre {a} e {b} é igual a {a/b}')
+
