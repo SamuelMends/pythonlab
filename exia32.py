1eaba72e3c0e251009a05cb3eb4df94fb8d1bcf5
SamuelMends <samuelqmendes@gmail.com>
2024-05-31 19:48:54 -0300

    Create exia32.py

---
+++ exia32.py
@@ -0,0 +1,5 @@
+# Lista: Crie uma estrutura de repetição para fazer a leitura de 5 números inteiros e os armazene dentro de uma lista. Após a leitura, crie outra estrutura de repetição para somar todos os valores digitados
+
+acu = 0
+for num in range(1,6):
+    n 
