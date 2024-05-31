b226ec1954ada904dcebff46d1c2ca96aac3fc4b
SamuelMends <samuelqmendes@gmail.com>
2024-05-31 18:26:22 -0300

    Update tupla.py

--- tupla.py
+++ tupla.py
@@ -4,3 +4,10 @@ tupla = ('Homo Sapiens', 'Canis familiaris', 'Felis Catus')
 
 for elemento in tupla:
     print(elemento)
+
+
+l1 = ['Homo Sapiens', 'Canis familiaris', 'Felis Catus']
+l2 = ['Humano', 'Cachorro', 'Gato']
+
+l3 = l1 + l2
+print(l3)
