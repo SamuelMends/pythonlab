ecc62fa23d4c197aea2de41c34e785439fb82be5
SamuelMends <samuelqmendes@gmail.com>
2024-05-31 20:36:16 -0300

    Update exia32.py

--- exia32.py
+++ exia32.py
@@ -6,6 +6,18 @@ for num in range(1,6): #Adicionamos o range de 5 repetições
     lista.append(n) #Usamos o Append para adicionar o elemento em uma lista
 print(lista)    
 
+#Existem duas formas de fazer a soma, manual e automática
+
+#Manual:
+som = 0 
+for c in range(len(lista)):
+    som += lista[c]
+print(f'Soma: {som}')
+
+#Automatica:
+import numpy as np #Importando o numpy e dando o nome de np
+np.array(lista).sum()
+
 
 # Dicionário: Crie um dicionário para armazenar o nome e a nota de 3 alunos, fazendo a leitura dos valores por meio de uma estrutura de repetição. Depois, crie uma nova estrutura de repetição para somar todas as notas e retornar a média
 
