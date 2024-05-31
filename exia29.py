7ebb82e25e9e1309b5b663dc3724ac16dc40a4de
SamuelMends <samuelqmendes@gmail.com>
2024-05-30 22:09:53 -0300

    Update exia29.py

--- exia29.py
+++ exia29.py
@@ -1,7 +1,14 @@
 #Soma com for 
-
+"""
 soma = 0 
 for numero in range (0,6):
     soma = soma + numero
     print(soma)
 print(soma)
+"""
+
+palavra = 'sorvete'
+for letra in palavra:
+    print(letra)
+if 'v' in palavra:
+    print('Achei a letra V')
