1b75510519a5d6f0e3731f746a7aaa24ebb19fdc
SamuelMends <samuelqmendes@gmail.com>
2024-05-31 20:11:33 -0300

    Update exia32.py

--- exia32.py
+++ exia32.py
@@ -1,8 +1,16 @@
 # Lista: Crie uma estrutura de repetição para fazer a leitura de 5 números inteiros e os armazene dentro de uma lista. Após a leitura, crie outra estrutura de repetição para somar todos os valores digitados
-
+"""
 acu = 0
 for num in range(1,6):
     n = int(input(f'Digite o {num}º número: '))
-    acu = acu + n
-l1 = [acu]    
-print(l1)
+    acu = acu + n   
+"""
+
+# Dicionário: Crie um dicionário para armazenar o nome e a nota de 3 alunos, fazendo a leitura dos valores por meio de uma estrutura de repetição. Depois, crie uma nova estrutura de repetição para somar todas as notas e retornar a média
+
+dici = {"Samanta": 8, "Fabricio": 9, "Ayrton": 7}
+dici.items()
+"""
+for lei, nota in dici():
+    print(lei)
+"""
