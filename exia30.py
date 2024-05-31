5f54c9d2d1370401627a8eeb5a274b387d5aa7a8
SamuelMends <samuelqmendes@gmail.com>
2024-05-30 22:48:58 -0300

    Update exia30.py

--- exia30.py
+++ exia30.py
@@ -2,16 +2,17 @@
 
 # Ler 5 notas e informar a média
 # Imprimir a tabuada do número 3 (3 x 1 = 1 – 3 x 10 = 30)
-"""
-nota = []
+
+nota = media = soma = 0
 
 for n in range(5):
     n1 = int(input(f'Digite sua nota {n+1}: '))
-    n1.append(nota)
-med = (nota)/5
+    soma = soma + n1
+med = (soma)/5
 print(f'Sua média é igual a {med}')
-"""
 
+
+"""
 notas = []
 
 for n in range(5):
@@ -19,4 +20,12 @@ for n in range(5):
     notas.append(nota)
 
 med = sum(notas) / len(notas)
+print(f'Sua média é igual a {med}')
+"""
+
+cont = 0 
+while cont < 5:
+    nota = int(input(f'Digite sua {cont+1}º nota: '))
+    cont +=1
+med = nota/5
 print(f'Sua média é igual a {med}')
