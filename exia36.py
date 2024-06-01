a7a4b50271f2d984431f482f0778075871f908e7
SamuelMends <samuelqmendes@gmail.com>
2024-05-31 22:45:33 -0300

    Create exia36.py

---
+++ exia36.py
@@ -0,0 +1,8 @@
+# Tratamento de erros e exceções
+
+#  NameError: variável nao foi definida
+#  TypeError: tipos de dados incompatíveis
+#  RuntimeError: erro de execução
+#  SyntaxError: sintaxe digitada é inválida e não reconhecida pelo interpretador
+#  ZeroDivisionError: divisão por zero
+#  IndexError: índice está fora da coleção
