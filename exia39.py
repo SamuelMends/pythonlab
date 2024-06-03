200b5b40519e9fbd4ff7395a1b36cb6352c59c55
SamuelMends <samuelqmendes@gmail.com>
2024-06-03 18:57:44 -0300

    Update exia39.py

--- exia39.py
+++ exia39.py
@@ -1,10 +1,11 @@
-# Crie uma classe chamada aluno com os seguintes atributos:
-# Nome
-# Nota 1
-# Nota 2
+#1) Crie uma classe chamada aluno com os seguintes atributos:
+# Nome:
+# Nota 01:
+# Nota 02:
 # Crie um construtor para a classe (__init__)
-# Crie as seguintes funções (métodos):
+#2) Crie as seguintes funções (métodos):
 # Calcula média, retornando a média aritmética entre as notas
 # Mostra dados, que somente imprime o valor de todos os atributos
 # Resultado, que verifica se o aluno está aprovado ou reprovado (se a média for maior ou igual a 6.0, o aluno está aprovado)
-# Crie dois objetos (aluno1 e aluno2) e teste as funções
+#3) Crie dois objetos (aluno1 e aluno2) e teste as funções
+
