b2704e864ff8ebb971c6787d090c1b2ab0e73b24
SamuelMends <samuelqmendes@gmail.com>
2024-06-03 18:17:01 -0300

    Update exi38.py

--- exi38.py
+++ exi38.py
@@ -5,16 +5,22 @@
 
 import re
 
+frase = 'Olá, meu CEP é de telefone é (42)0000-0000'
+
+resultado = re.search('\(\d{2}\)\d{4,5}-\d{4}', frase)
+if resultado:
+    print("Número de telefone encontrado:", resultado.group())
+else:
+    print("Número de telefone não encontrado.")
+
+
 frase = 'Olá, meu CEP é de telefone é 13185-541'
 
 resultado = re.search('\d{0,5}-\d{0,4}', frase)
 if resultado:
     print("CEP Encontrado:", resultado.group())
 else:
-    print("Número de telefone não encontrado.")
-
-
-# frase = 'Olá, meu CEP é 13185-541'
+    print("CEP não encontrado.")
 
 
 # CEP 12345-000
