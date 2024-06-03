0b6c198cb00178d3a0c318fdbe35c5ccd2f46656
SamuelMends <samuelqmendes@gmail.com>
2024-06-02 23:51:50 -0300

    Update expregulares.py

--- expregulares.py
+++ expregulares.py
@@ -3,3 +3,7 @@ import re
 frase = 'Olá, meu número de telefone é (42)0000-0000'
 
 resultado = re.search('\(\d{2}\)\d{4,5}-\d{4}', frase)
+if resultado:
+    print("Número de telefone encontrado:", resultado.group())
+else:
+    print("Número de telefone não encontrado.")
