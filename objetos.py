64b8f69cabd6542487653026361da89208a39d51
SamuelMends <samuelqmendes@gmail.com>
2024-06-03 18:51:30 -0300

    Update objetos.py

--- objetos.py
+++ objetos.py
@@ -9,6 +9,15 @@ class Triangulo:
 
     def area(self):
         return (self.base * self.altura)/2
+    
+
+    def tipo(self):
+        if self.lado1 > self.lado2 + self.lado3:
+            return'Não é um triângulo'
+        elif self.lado1 == self.lado2 or self.lado1 == self.lado3 or self.lado1 == self.lado2:
+            return 'Triângulo Isóceles'
+        else:
+            return 'Outro tipo de Triângulo'
 
 
 t1 = Triangulo(2, 1, 3, 4, 3)
@@ -18,3 +27,5 @@ print(t1.area())
 
 t2 = Triangulo(8, 8, 8, 16, 9)
 print(t2.lado1, t2.lado2, t2.lado3, t2.base, t2.altura)
+
+print(t2.area())
