a60593cae43c30aadc96ded7561989aa7ddd65d1
SamuelMends <samuelqmendes@gmail.com>
2024-06-03 19:12:39 -0300

    Update exia39.py

--- exia39.py
+++ exia39.py
@@ -9,3 +9,33 @@
 # Resultado, que verifica se o aluno está aprovado ou reprovado (se a média for maior ou igual a 6.0, o aluno está aprovado)
 #3) Crie dois objetos (aluno1 e aluno2) e teste as funções
 
+
+class alunos:
+    def __init__(self, nome, nota1, nota2):
+        self.nome = nome
+        self.nota1 = nota1
+        self.nota2 = nota2
+    
+
+    def med(self):
+        return (self.nota1 + self.nota2) / 2
+    
+
+    def total(self):
+        return (self.nota1 + self.nota2)
+    
+    def resultado(self):
+        if self.med() >= 6.0:
+            print(f'O Aluno {self.nome} está aprovado com média {self.med()}')
+        else:
+            print('Aluno reprovado.')
+
+
+a1 = alunos("Samuel", 10, 10)
+
+print('Média geral', a1.med())
+a1.resultado()
+print('Pontos Totais', a1.total())
+
+# a2 = alunos(Jucelino, 3, 4)
+
