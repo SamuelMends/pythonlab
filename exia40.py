a6ceaa3883f8bae0f262e7deb28c55ef277a4d18
SamuelMends <samuelqmendes@gmail.com>
2024-06-03 23:24:08 -0300

    Create exia40.py

---
+++ exia40.py
@@ -0,0 +1,13 @@
+#Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido. 
+
+resp = float(input('Digite uma nota entre 0 e 10: '))
+while resp > 10:
+    print('Número incorreto! Digite um valor válido! ')
+    resp = float(input('Digite uma nota entre 0 e 10 novamente: '))
+    if resp <= 10:
+        print(f'Está é sua nota {resp}')
+        break 
+
+
+#Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações. 
+
