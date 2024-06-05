b9ac6f32bdbed5ca490ebe6dc27276badbc55bec
SamuelMends <samuelqmendes@gmail.com>
2024-06-04 23:09:26 -0300

    Update exia44.py

--- exia44.py
+++ exia44.py
@@ -1,5 +1,10 @@
 #Faça um programa que leia 5 números e informe o maior número. 
 
+import numpy as np
+
+lista = []
 c = 0
 for c in range(1,6):
+    num = int(input('Digite um número: '))
+    lista.append(num)
     print(c)
